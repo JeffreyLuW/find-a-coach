65b7d8b886dda9bb22f454755d39895da61caf0d
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-13 17:42:23 +0800

    build-coachDetail-component-(not-fininshed)

--- client/src/pages/coaches/CoachDetail.vue
+++ client/src/pages/coaches/CoachDetail.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>
-    <div>Coach detail</div>
-    <router-link to="/coaches/c6/contact">Go to ContactCoach page</router-link>
-    <router-view></router-view>
-    </div>
+<div>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  </div>
 </template>
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+};
+</script>
