99436885a3d091834fa625842dec1111434d477c
Jeffrey Lu <146838940+JeffreyLuW@users.noreply.github.com>
2023-12-13 17:56:50 +0800

    build-coachDetail-component-add-areas

--- client/src/pages/coaches/CoachDetail.vue
+++ client/src/pages/coaches/CoachDetail.vue
@@ -1,20 +1,31 @@
 <template>
-<div>
-  <section>
-    <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Interested? Reach out now!</h2>
-        <base-button link :to="contactLink">Contact</base-button>
-      </header>
-      <router-view></router-view>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        ></base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
   </div>
 </template>
 <script>
@@ -34,6 +45,9 @@ export default {
     },
     rate() {
       return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
     },
     contactLink() {
       return this.$route.path + '/' + this.id + '/contact';
